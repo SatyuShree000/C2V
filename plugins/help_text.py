8697afca23f463e473c002046ca16f349c5b47a3
Satyu <70142319+SatyuShree000@users.noreply.github.com>
2020-10-21 01:26:53 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -11,6 +11,15 @@ logger = logging.getLogger(__name__)
 import os
 import sqlite3
 
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
